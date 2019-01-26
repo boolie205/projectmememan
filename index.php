0adecf759ec9b116f2d993b6c925c1f631252d24
Lewis <boolie205@gmail.com>
2019-01-26 18:44:58 +0000

    semantic ui and jquery added

---
+++ index.php
@@ -0,0 +1,19 @@
+<head>
+<link rel="stylesheet" type="text/css" href="/styles/Semantic-ui/semantic.min.css">
+</head>
+
+<?php include './php-parts/nav.php';?>
+
+
+
+
+
+
+
+
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+
+<script src="/styles/Semantic-ui/semantic.min.js"></script>
---
+++ readme.txt
@@ -0,0 +1,4 @@
+memes.mememan
+
+What is this ? 
+Mail a meme allows you to unironically snail mail memes to your buddys. 
